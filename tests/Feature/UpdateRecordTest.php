674e5127e91366478776c0e19e1e61394a111ebc
Vrian7 <i-am-brian@hotmail.com>
2018-08-20 09:13:46 -0400

    test correction

--- tests/Feature/UpdateRecordTest.php
+++ tests/Feature/UpdateRecordTest.php
@@ -20,23 +20,17 @@ class UpdateRecordTest extends TestCase
         $data = factory(\App\Record::class)->make();
 		$data_update = [
         'uuid' => $data->uuid,
-		'name' => $data->name,
+		'name' => 'bbr'.$data->name,
 		'description' => $data->description,
 		'code' => $data->code,
 		'status_id' => $data->status_id
 		];
         //$record =  factory(App\Record::class,100)->create();
 
-        $recordRepo = new \App\Record($data_update);
+        $recordRepo = new \App\Record($data_update);        
 
-        $response = $this->update('/record',$data_update);
-        //$update = $recordRepo->updateRecord($data);
+        $response = $this->json('PUT', '/record/1', $data_update);
 
-        $this->assertTrue($update);
-        $this->assertEquals($data['uuid'], $carousel->uuid);
-        $this->assertEquals($data['name'], $carousel->name);
-        $this->assertEquals($data['description'], $carousel->description);
-        $this->assertEquals($data['code'], $carousel->code);
-        $this->assertEquals($data['status_id'], $carousel->status_id);
+        $response->assertStatus(200);            
     }
 }
